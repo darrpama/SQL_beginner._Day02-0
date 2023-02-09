e7920da81b140259efbc83d7641d4afb1d55e33f
darrpama <darrpama@student.21-school.ru>
2023-02-09 22:35:25 +0300

    fixed ex01

--- src/ex01/day02_ex01.sql
+++ src/ex01/day02_ex01.sql
@@ -1,4 +1,4 @@
-SELECT CAST(dates as DATE) FROM ('2022-01-01', '2022-01-10', ) as dates
+SELECT CAST(dates as DATE) FROM generate_series('2022-01-01', '2022-01-10', '1 day'::interval) as dates
 LEFT JOIN (SELECT * FROM person_visits WHERE person_visits.person_id = 1 OR person_visits.person_id = 2) as info
 ON info.visit_date = dates WHERE info.visit_date IS NULL
 ORDER BY dates;
